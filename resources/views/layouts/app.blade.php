1fcdd8775ce98859a35a0e5644c9da39e28ea972
Benjamin Strilziw <info@internerz.de>
2016-12-01 18:08:16 +0100

    add favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,11 @@
     <!-- Styles -->
     <link href="/css/style.css" rel="stylesheet">
 
+    <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
     <!-- Scripts -->
     <script>
         window.Laravel = <?php echo json_encode([
