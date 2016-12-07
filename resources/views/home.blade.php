b8636d3211483474a81d4202939b1b332d79099b
Benjamin Strilziw <info@internerz.de>
2016-12-07 22:45:23 +0100

    add nicer home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Home</div>
 
-                <div class="panel-body">
-                    You are logged in!
+                    <div class="panel-body">
+                        @if (Auth::guest())
+                            <ul class="nav nav-pills nav-stacked">
+                                <li role="presentation"><a href="{{ url('/login') }}">Login</a></li>
+                                <li role="presentation"><a href="{{ url('/register') }}">Register</a></li>
+                            </ul>
+                        @else
+                            <ul class="nav nav-pills nav-stacked">
+                                <li><a href="/game">List games</a></li>
+                                <li><a href="/game/create">Create game</a></li>
+                                @if (Auth::user()->admin == true)
+                                    <li><a href="/modes">Modes</a></li>
+                                @endif
+                            </ul>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
