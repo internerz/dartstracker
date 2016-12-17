2779750fe3350c8adefd27e9e9df2547341b61c1
Arcraith <tobias.schweisfurth@web.de>
2016-12-17 14:26:48 +0100

    Insert foreign key constraint into Game_orders table

---
+++ database/migrations/2016_12_17_164549_update_game_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateGameOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('game_orders', function (Blueprint $table) {
+            $table->integer('state_id')->unsigned();
+            $table->foreign('state_id')->references('id')->on('states');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
